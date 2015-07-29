d9b5d7387187d9abea81b4f02a1d49e833c6da8a
Danut Chereches <load.runner@weednet.ro>
2015-07-29 22:34:49 +0300

    implemented very basic invoice functions

--- main.js
+++ main.js
@@ -10,12 +10,12 @@ app.config(['$routeProvider', function($routeProvider) {
 	.when('/', {
 		templateUrl : 'templates/index.html',
 		controller : 'IndexController'
-	}).when('/clients', {
-		templateUrl : 'templates/clients/index.html',
-		controller : 'ClientsController'
 	}).when('/login', {
 		templateUrl : 'templates/login.html',
 		controller : 'LoginController'
+	}).when('/clients', {
+		templateUrl : 'templates/clients/index.html',
+		controller : 'ClientsController'
 	}).when('/clients/show/:id', {
 		templateUrl : 'templates/clients/get.html',
 		controller : 'GetClientController'
@@ -27,19 +27,16 @@ app.config(['$routeProvider', function($routeProvider) {
 		controller : 'PutClientController'
 	}).when('/invoices', {
 		templateUrl : 'templates/invoices/index.html',
-		controller : 'ClientsController'
-	}).when('/invoices/show', {
+		controller : 'InvoicesController'
+	}).when('/invoices/show/:id', {
 		templateUrl : 'templates/invoices/get.html',
-		controller : 'GetClientController'
+		controller : 'GetInvoiceController'
 	}).when('/invoices/add', {
 		templateUrl : 'templates/invoices/post.html',
-		controller : 'PostClientController'
-	}).when('/invoices/edit', {
+		controller : 'PostInvoiceController'
+	}).when('/invoices/edit/:id', {
 		templateUrl : 'templates/invoices/put.html',
-		controller : 'PutClientController'
-	}).when('/invoices/delete', {
-		templateUrl : 'templates/invoices/delete.html',
-		controller : 'DeleteClientController'
+		controller : 'PutInvoiceController'
 	}).when('/receipts', {
 		templateUrl : 'templates/receipts/index.html',
 		controller : 'ClientsController'
@@ -306,7 +303,7 @@ app.controller('IndexController', function($scope, $location) {
 	$scope.message = 'Index';
 });
 
-
+//// CLIENTS
 app.factory('Client', function($resource) {
 	return $resource('http://10.10.0.3/api/clients/:_id', {
 		_id : "@_id"
@@ -390,6 +387,96 @@ app.controller('PutClientController', ['$scope', '$location', '$routeParams', 'C
 	$scope.submit = function () {
 		$scope.client.$save(function () {
 			$location.path('/clients');
+		});
+	};
+}]);
+
+//// INVOICES
+app.factory('Invoice', function($resource) {
+	return $resource('http://10.10.0.3/api/invoices/:_id', {
+		_id : "@_id"
+	}, {
+		get : {
+			method  : 'GET',
+			headers : {
+				'Authorization' : 'Bearer ' + localStorage.access_token
+			}
+		}, insert : {
+			method  : 'POST',
+			headers : {
+				'Authorization' : 'Bearer ' + localStorage.access_token
+			}
+		}, save : {
+			method  : 'PUT',
+			headers : {
+				'Authorization' : 'Bearer ' + localStorage.access_token
+			}
+		}, remove : {
+			method  : 'DELETE',
+			headers : {
+				'Authorization' : 'Bearer ' + localStorage.access_token
+			}
+		}, query : {
+			method  : 'GET',
+			isArray : true,
+			headers : {
+				'Authorization' : 'Bearer ' + localStorage.access_token
+			}
+		}
+	});
+});
+
+app.controller('InvoicesController', ['$scope', '$location', 'Invoice', function($scope, $location, Invoice) {
+	$scope.message = 'This is list invoices screen';
+	$scope.menuClass = function (page) {
+		var current = $location.path();
+		return current.match('^\/invoices(\/)?'+page+'$') ? "active" : "";
+	};
+	$scope.invoices = Invoice.query();
+	$scope.remove = function (id) {
+		Invoice.remove({ _id : id }, function (r) {
+			console.log(r);
+			for (var i = 0; i < $scope.invoices.length; i++)
+				if ($scope.invoices[i]._id == id) {
+					$scope.invoices.splice(i, 1);
+					break;
+				}
+		});
+	};
+}]);
+
+app.controller('GetInvoiceController', ['$scope', '$location', '$routeParams', 'Invoice', function($scope, $location, $routeParams, Invoice) {
+	$scope.message = 'This is show invoice screen';
+	$scope.invoice = Invoice.get({_id : $routeParams.id});
+}]);
+
+app.controller('PostInvoiceController', ['$scope', '$location', 'Invoice', 'Client', function($scope, $location, Invoice, Client) {
+	$scope.message = 'This is add invoice screen';
+	$scope.clients = Client.query();
+	$scope.submit = function () {
+		console.log('xx', $scope);
+		if (!$scope.number || $scope.number.length < 1 || !$scope.client_id)
+			return;
+		
+		var invoice = new Invoice({
+			client_id : $scope.client_id,
+			number       : $scope.number
+		});
+		invoice.$insert(function (res) {
+			if (res.$resolved !== true)
+				return;
+			
+			$location.path('/invoices');
+		});
+	};
+}]);
+
+app.controller('PutInvoiceController', ['$scope', '$location', '$routeParams', 'Invoice', function($scope, $location, $routeParams, Invoice) {
+	$scope.message = 'This is edit invoice screen';
+	$scope.invoice = Invoice.get({_id : $routeParams.id});
+	$scope.submit = function () {
+		$scope.invoice.$save(function () {
+			$location.path('/invoices');
 		});
 	};
 }]);
